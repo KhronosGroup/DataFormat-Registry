9a57763ebfa74fd7c99291c5c9730d5e22f7ef82
Jon Leech <oddhack@sonic.net>
2016-12-12 02:29:55 -0800

    Populate git registry from SVN

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # DataFormat-Registry
-Khronos Data Format Specification
+
+The DataFormat-Registry repository contains published Khronos Data Format
+Specifications and header files.
+
+It is also used as a backing store for the web view of the registry at
+https://www.khronos.org/registry/dataformat/ ; commits to the master branch
+of this repository will be reflected there.
+
+In the past, the DataFormat registry was maintained in a public Subversion
+repository. The history in that repository has not been imported to github,
+but it is still available at
+https://cvs.khronos.org/svn/repos/registry/trunk/public/dataformat/ .
+
+Interesting files in this repository include:
+
+* index.php - toplevel index page for the web view. This relies on PHP
+  include files found elsewhere on www.khronos.org and so is not very useful
+  in isolation.
+* api/ - DataFormat header files
+* specs/ - DataFormat specification documents.
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+$static_title = 'Khronos Data Format Specification Registry';
+$static_breadcrumb = array(
+    '/registry/' => 'Registry',
+    'NOLINK' => 'Khronos Data Format Specification Registry'
+);
+include_once("../../assets/static_pages/khr_page_top.php");
+?>
+
+<h1 class="pagetitle">Khronos Data Format Specification Registry</h1>
+<div id="mainformat">
+
+<h1 style="text-align:center"> Khronos Data Format Specification Registry </h1>
+
+<p> The Khronos Data Format Specification registry contains the
+    specification of the Khronos data format descriptor structure and
+    associated headers. </p>
+
+<h6> Core Data Format Specification and Headers </h6>
+
+<p> The current version is 1.1 revision 6, June, 2016. </p>
+
+<ul>
+<li> <a href="specs/1.1/dataformat.1.1.html"> Khronos Data Format 1.1
+     Specification (HTML) </a>. The specification is also available in
+     these additional formats:
+
+     <ul>
+     <li> <a href="specs/1.1/dataformat.1.1.pdf"> PDF </a> </li>
+     </ul>
+<li> <a href="api/1.1/khr_df.h"> header file </a> <tt>(khr_df.h)</tt> </li>
+</ul>
+
+
+<p> The previous version is 1.0 revision 4, July 10, 2015. </p>
+
+<ul>
+<li> <a href="specs/1.0/dataformat.1.0.html"> Khronos Data Format 1.0
+     Specification (HTML) </a>. The specification is also available in
+     these additional formats:
+
+     <ul>
+     <li> <a href="specs/1.0/dataformat.1.0.inline.html"> HTML (single file
+          with inline images) </a> </li>
+     <li> <a href="specs/1.0/chunked/index.html"> HTML (chunked) </a> </li>
+     <li> <a href="specs/1.0/dataformat.1.0.pdf"> PDF </a> </li>
+     <li> <a href="specs/1.0/dataformat.1.0.html.zip"> HTML zip file </a>
+          for offline reading.
+     <li> <a href="specs/1.0/dataformat.1.0.chunked.zip"> HTML (chunked) zip
+          file </a> for offline reading.
+     </ul>
+<li> <a href="api/1.0/khr_df.h"> header file </a> <tt>(khr_df.h)</tt> </li>
+</ul>
+
+<h6> Providing Feedback on the Registry </h6>
+
+<p> Khronos welcomes comments and bug reports. To provide feedback on the
+    DataFormat registry itself (such as reporting missing content, bad
+    links, etc.), file an issue in the <a
+    href="https://github.com/KhronosGroup/DataFormat-Registry/issues">
+    DataFormat-Registry </a> Github project. </p>
+
+<p> For the Data Format Specification and headers, file an issue in the <a
+    href="https://github.com/KhronosGroup/DataFormat/issues"> DataFormat
+    </a> Github project. </p>
+
+<?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
+</body>
+</html>
