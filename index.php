5fcb959daf2dc8f3b7a5d952ca07cb1ebdc703d0
Andrew Garrard <a.garrard@samsung.com>
2017-09-14 16:50:33 +0100

    Updated to Khronos Data Format Specification 1.2.

--- index.php
+++ index.php
@@ -8,47 +8,75 @@ include_once("../../assets/static_pages/khr_page_top.php");
     specification of the Khronos data format descriptor structure and
     associated headers. </p>
 
-<h6> Core Data Format Specification and Headers </h6>
+<hr />
 
-<p> The current version is 1.1 revision 8, June, 2017. </p>
-
-<ul>
-<li> <a href="specs/1.1/dataformat.1.1.html"> Khronos Data Format 1.1
-     Specification (HTML) </a>. The specification is also available in
-     these additional formats:
-
-     <ul>
-     <li> <a href="specs/1.1/dataformat.1.1.pdf"> PDF </a> </li>
-     </ul>
-<li> <a href="api/1.1/khr_df.h"> header file </a> <tt>(khr_df.h)</tt> </li>
-</ul>
-
-
-<p> The previous version is 1.0 revision 4, July 10, 2015. </p>
+<h2>Current core Khronos Data Format Specification and Headers</h2>
 
 <ul>
-<li> <a href="specs/1.0/dataformat.1.0.html"> Khronos Data Format 1.0
-     Specification (HTML) </a>. The specification is also available in
-     these additional formats:
-
-     <ul>
-     <li> <a href="specs/1.0/dataformat.1.0.inline.html"> HTML (single file
-          with inline images) </a> </li>
-     <li> <a href="specs/1.0/chunked/index.html"> HTML (chunked) </a> </li>
-     <li> <a href="specs/1.0/dataformat.1.0.pdf"> PDF </a> </li>
-     <li> <a href="specs/1.0/dataformat.1.0.html.zip"> HTML zip file </a>
-          for offline reading.
-     <li> <a href="specs/1.0/dataformat.1.0.chunked.zip"> HTML (chunked) zip
-          file </a> for offline reading.
-     </ul>
-<li> <a href="api/1.0/khr_df.h"> header file </a> <tt>(khr_df.h)</tt> </li>
+<li> <b>Khronos Data Format Specification version 1.2 revision 0, updated September 2017</b> <br />
+   <ul>
+     <li> <a href="specs/1.2/dataformat.1.2.pdf">Specification as PDF</a> </li>
+     <li> <a href="specs/1.2/dataformat.1.2.html">Specification as HTML</a>
+          for online browsing</li>
+     <li> <a href="specs/1.2/dataformat.1.2.html.zip">Specification as HTML zip file</a>
+          containing all referenced images for download</li>
+     <li> <a href="specs/1.2/dataformat.1.2.inline.html">Specification as HTML single file with inline images</a>
+          for ease of sharing</li>
+     <li> <a href="specs/1.2/dataformat.1.2.inline.html.zip">Specification as zipped HTML single file with inline images</a>
+          for sharing at reduced size <br />
+          <ul>
+              <li><b>Note:</b> HTML versions of the Khronos Data Format Specification 1.2
+                rely on access to <a href="http://www.mathjax.org">MathJax</a> to
+                render formulae.<br />
+                For fully off-line access, please use the PDF.</li>
+          </ul></li>
+     <li> <a href="api/1.2/khr_df.h">Header file</a> <tt>(khr_df.h)</tt> </li>
+     </ul> </li>
 </ul>
 
-<h6> Providing Feedback on the Registry </h6>
+<hr />
+
+<h2>Older core Khronos Data Format Specification and Headers</h2>
+
+<ul>
+  <li> <b>Khronos Data Format Specification version 1.1 revision 8, June 2017</b> <br />
+     <ul>
+     <li> <a href="specs/1.1/dataformat.1.1.pdf">Specification as PDF </a> </li>
+     <li> <a href="specs/1.1/dataformat.1.1.html">Specification as HTML</a> for online browsing </li>
+     <li> <a href="specs/1.1/dataformat.1.1.html.zip">Specification as HTML zip file</a>
+          containing all referenced images for download<br />
+          <ul>
+              <li><b>Note:</b> HTML versions of the Khronos Data Format Specification 1.1
+                rely on access to <a href="http://www.mathjax.org">MathJax</a> to
+                render formulae.<br />
+                For fully off-line access, please use the PDF.</li>
+          </ul></li>
+     <li> <a href="api/1.1/khr_df.h"> Header file</a> <tt>(khr_df.h)</tt> </li>
+     </ul></li>
+  <li> <b>Khronos Data Format Specification version 1.0 revision 4, July 2015</b> <br />
+     <ul>
+     <li> <a href="specs/1.0/dataformat.1.0.pdf">Specification as PDF</a> </li>
+     <li> <a href="specs/1.0/dataformat.1.0.html">Specification as HTML</a>
+          for online browsing </li>
+     <li> <a href="specs/1.0/dataformat.1.0.inline.html">Specification as single HTML file
+          with inline images</a> for ease of sharing </li>
+     <li> <a href="specs/1.0/chunked/index.html">Specification as chunked HTML</a>
+          for online browsing </li>
+     <li> <a href="specs/1.0/dataformat.1.0.html.zip">Specification as HTML zip file</a>
+          for offline reading </li>
+     <li> <a href="specs/1.0/dataformat.1.0.chunked.zip">Specification as chunked HTML zip
+          file</a> for offline reading </li>
+     <li> <a href="api/1.0/khr_df.h">Header file</a> <tt>(khr_df.h)</tt> </li>
+     </ul></li>
+</ul>
+
+<hr />
+
+<h2> Providing Feedback on the Registry </h6>
 
 <p> Khronos welcomes comments and bug reports. To provide feedback on the
-    DataFormat registry itself (such as reporting missing content, bad
-    links, etc.), file an issue in the <a
+    Data Format Specification Registry itself (such as reporting missing
+    content, bad links, etc.), file an issue in the <a
     href="https://github.com/KhronosGroup/DataFormat-Registry/issues">
     DataFormat-Registry </a> Github project. </p>
 
